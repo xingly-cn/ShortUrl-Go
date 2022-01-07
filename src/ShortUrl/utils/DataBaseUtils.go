cf787a47b6d087190b896e758263b86df236ad82
xingly-cn <987599519@qq.com>
2022-01-07 19:05:20 +0800

    优化 SQL 和 Redis 工具类

--- src/ShortUrl/utils/DataBaseUtils.go
+++ src/ShortUrl/utils/DataBaseUtils.go
@@ -1,16 +1,29 @@
 package utils
 
 import (
+	"github.com/go-redis/redis"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"log"
 )
 
-// 获取数据库对象
 func GetSQL() *gorm.DB {
 	db, err := gorm.Open("mysql", "root:XNXxnx520@(8.142.199.134:3306)/shorturl?charset=utf8mb4")
 	if err != nil {
 		log.Println("连接数据库异常：", err)
 	}
 	return db
+}
+
+func GetRedis() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "175.27.243.243:6379",
+		Password: "213879",
+		DB:       0,
+	})
+	_, err := rdb.Ping().Result()
+	if err != nil {
+		log.Println("Redis异常：", err)
+	}
+	return rdb
 }
