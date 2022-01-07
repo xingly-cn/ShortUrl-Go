0466e4f787d09bd19dbabcef6c97bb0ec6ed0db3
xingly-cn <987599519@qq.com>
2022-01-07 14:04:47 +0800

    数据库对象工具类

--- src/ShortUrl/utils/DataBaseUtils.go
+++ src/ShortUrl/utils/DataBaseUtils.go
@@ -7,6 +7,6 @@ import (
 
 // 获取数据库对象
 func GetSQL() (*gorm.DB, error) {
-	db, err := gorm.Open("mysql", "root:XNXxnx520@(8.142.199.134:3306)/teapic?charset=utf8mb4")
+	db, err := gorm.Open("mysql", "root:XNXxnx520@(8.142.199.134:3306)/shorturl?charset=utf8mb4")
 	return db, err
 }
