376003190dfd56cbe212a77619bd406a1b9dfc2d
xingly-cn <987599519@qq.com>
2022-01-07 14:07:19 +0800

    优化数据库工具类

--- src/ShortUrl/utils/DataBaseUtils.go
+++ src/ShortUrl/utils/DataBaseUtils.go
@@ -3,10 +3,14 @@ package utils
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
 )
 
 // 获取数据库对象
-func GetSQL() (*gorm.DB, error) {
+func GetSQL() *gorm.DB {
 	db, err := gorm.Open("mysql", "root:XNXxnx520@(8.142.199.134:3306)/shorturl?charset=utf8mb4")
-	return db, err
+	if err != nil {
+		log.Println("连接数据库异常：", err)
+	}
+	return db
 }
